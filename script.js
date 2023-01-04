f9abdeba16a333a69952516a75c14957d8e523cd
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-01-04 18:59:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5!");
+// } else {
+//     console.log("Your number is greater than 0.5!");
+// }
+// console.log(random);
+
+
+
+
+// const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+// if (dayOfWeek === 'monday') {
+//     console.log("UGHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === 'saturday') {
+//     console.log("YAY I LOVE SATURDAYS");
+// } else if (dayOfWeek === 'friday') {
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// } else {
+//     console.log("MEH");
+// }
+
+
+
+
+const age = 89;
+if (age < 5) {
+    console.log("You are a baby. You get in for free!");
+} else if (age < 10) {
+    console.log("You are a child. You pay $10");
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20");
+} else {
+    console.log("You are a senior. You pay $10");
+}
+
+
+
